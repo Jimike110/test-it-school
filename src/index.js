40aabac5aceb9ec238dbbe7f5d158fcfb0b854e3
Jimike110 <oladoyemike@gmail.com>
2025-04-21 18:51:53 +0300

    Removed Suspense wrapper as it was overflowing the page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React, { Suspense } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import './i18n'; 
+import "./i18n";
 import App from "./App";
 import { BrowserRouter } from "react-router";
 
@@ -9,9 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <Suspense fallback={<div>Loading...</div>}>
-    <App />
-  </Suspense>,
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
