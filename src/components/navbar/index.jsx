732a3ba6ebdc57a19580c087f10ec0deb49359da
Jimike110 <oladoyemike@gmail.com>
2025-04-21 11:54:13 +0300

    Optimized Navbar component code

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,24 +1,25 @@
-import { Flex, Image } from 'antd'
 import React from 'react';
-import { Typography } from 'antd';
+import { Link } from 'react-router';
+import { Image, Typography } from 'antd';
+import './navbar.css';
 
 const { Text } = Typography;
 
-const NavBar = () => {
+export default function NavBar() {
   return (
-    <div style={{ fontWeight: 'bold' }}>
-        <Flex style={{ padding: '10px 20px'}} justify='space-between' align='center'>
-            <Image src=''  />
-            <span>
-                <Text>Язык сайта: </Text>
-                <select style={{ border: 'none' }}>
-                    <option defaultChecked value={'ru'}>Русский</option>
-                    <option value={'eng'}>Английский</option>
-                </select>
-            </span>
-        </Flex>
-    </div>
-  )
+    <nav className="navbar">
+      <div className="navbar__brand">
+        <Link to={'/'}>
+          <Image preview={false} src="/logo.png" alt="Logo" height={40} />
+        </Link>
+      </div>
+      <div className="navbar__controls">
+        <Text className="navbar__lang-label">Язык сайта:</Text>
+        <select className="navbar__lang-switcher">
+          <option value="ru">Русский</option>
+          <option value="en">English</option>
+        </select>
+      </div>
+    </nav>
+  );
 }
-
-export default NavBar
