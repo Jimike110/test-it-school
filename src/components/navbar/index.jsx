17cdefab22ab10715de27c2b95e7bf2fa0e12304
Jimike110 <oladoyemike@gmail.com>
2025-04-21 12:51:29 +0300

    Added container to keep navbar content under max-width

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -8,17 +8,19 @@ const { Text } = Typography;
 export default function NavBar() {
   return (
     <nav className="navbar">
+      <div className='container'>
       <div className="navbar__brand">
         <Link to={'/'}>
-          <Image preview={false} src="/logo.png" alt="Logo" height={40} />
+          <Image preview={false} src="./assets/images/logo.png" alt="Logo" height={40} />
         </Link>
       </div>
       <div className="navbar__controls">
-        <Text className="navbar__lang-label">Язык сайта:</Text>
+        <Text className="navbar__lang-label">ЯЗЫК САЙТА:</Text>
         <select className="navbar__lang-switcher">
-          <option value="ru">Русский</option>
-          <option value="en">English</option>
+          <option value="ru">РУССКИЙ</option>
+          <option value="en">ENGLISH</option>
         </select>
+      </div>
       </div>
     </nav>
   );
