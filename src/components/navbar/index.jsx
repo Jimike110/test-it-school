aca9e5a007668ca46134f4e8197498ef043f7400
Jimike110 <oladoyemike@gmail.com>
2025-04-21 13:56:07 +0300

    Made all site text use i18n localizations

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,26 +1,44 @@
 import React from 'react';
-import { Link } from 'react-router';
+import { Link } from 'react-router-dom';
 import { Image, Typography } from 'antd';
+import { useTranslation } from 'react-i18next';
 import './navbar.css';
 
 const { Text } = Typography;
 
 export default function NavBar() {
+  const { i18n, t } = useTranslation();
+
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+  };
+
   return (
     <nav className="navbar">
-      <div className='container'>
-      <div className="navbar__brand">
-        <Link to={'/'}>
-          <Image preview={false} src="./assets/images/logo.png" alt="Logo" height={40} />
-        </Link>
-      </div>
-      <div className="navbar__controls">
-        <Text className="navbar__lang-label">ЯЗЫК САЙТА:</Text>
-        <select className="navbar__lang-switcher">
-          <option value="ru">РУССКИЙ</option>
-          <option value="en">ENGLISH</option>
-        </select>
-      </div>
+      <div className="container">
+        <div className="navbar__brand">
+          <Link to="/">
+            <Image
+              preview={false}
+              src="/assets/images/logo.png"
+              alt="Logo"
+              height={40}
+            />
+          </Link>
+        </div>
+
+        <div className="navbar__controls">
+          <Text className="navbar__lang-label">{t('language_switcher_label')}:</Text>
+
+          <select
+            className="navbar__lang-switcher"
+            value={i18n.language}
+            onChange={(e) => changeLanguage(e.target.value)}
+          >
+            <option value="ru">РУССКИЙ</option>
+            <option value="en">ENGLISH</option>
+          </select>
+        </div>
       </div>
     </nav>
   );
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router';
+import { Link } from 'react-router-dom';
 import NavBar from '../../components/navbar';
 import RobotEmbed from '../../components/robot/RobotEmbed';
 import './home.css';
+import { useTranslation } from 'react-i18next';
 
 export default function Home() {
+  const { t } = useTranslation();
+
   return (
     <div className="home">
+      <title>{t('page_title')}</title>
       <NavBar className="home__nav" />
 
       <main className="home__main">
@@ -17,14 +21,14 @@ export default function Home() {
 
           <div className="home__content">
             <h2 className="home__title">
-              Запишитесь на пробное занятие в IT‑Школе
+              {t('hero_text')}
             </h2>
             <div className="home__actions">
               <button className="button button--primary">
-                НАЧАТЬ
+                {t('start_button_text')}
               </button>
               <Link to="/" className="button button--secondary">
-                У МЕНЯ УЖЕ ЕСТЬ АККАУНТ
+                {t('has_account_button_text')}
               </Link>
             </div>
           </div>
